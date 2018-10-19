4cc46dd3ad741e3fc745ed6b050a81dd38ba649c
Daksh <hello@dak.sh>
2018-10-19 14:49:20 +0530

    whether instead of wether

--- src/vs/platform/theme/common/themeService.ts
+++ src/vs/platform/theme/common/themeService.ts
@@ -1,140 +1,311 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+	/*---------------------------------------------------------------------------------------------
 
-import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
-import { Color } from 'vs/base/common/color';
-import { IDisposable, toDisposable } from 'vs/base/common/lifecycle';
-import * as platform from 'vs/platform/registry/common/platform';
-import { ColorIdentifier } from 'vs/platform/theme/common/colorRegistry';
-import { Event, Emitter } from 'vs/base/common/event';
-import { IEnvironmentService } from 'vs/platform/environment/common/environment';
+	 *  Copyright (c) Microsoft Corporation. All rights reserved.
 
-export const IThemeService = createDecorator<IThemeService>('themeService');
+	 *  Licensed under the MIT License. See License.txt in the project root for license information.
 
-export interface ThemeColor {
-	id: string;
-}
+	 *--------------------------------------------------------------------------------------------*/
 
-export function themeColorFromId(id: ColorIdentifier) {
-	return { id };
-}
+	
 
-// theme icon
-export interface ThemeIcon {
-	readonly id: string;
-}
 
-export const FileThemeIcon = { id: 'file' };
-export const FolderThemeIcon = { id: 'folder' };
+	import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
 
-// base themes
-export const DARK: ThemeType = 'dark';
-export const LIGHT: ThemeType = 'light';
-export const HIGH_CONTRAST: ThemeType = 'hc';
-export type ThemeType = 'light' | 'dark' | 'hc';
+	import { Color } from 'vs/base/common/color';
 
-export function getThemeTypeSelector(type: ThemeType): string {
-	switch (type) {
-		case DARK: return 'vs-dark';
-		case HIGH_CONTRAST: return 'hc-black';
-		default: return 'vs';
-	}
-}
+	import { IDisposable, toDisposable } from 'vs/base/common/lifecycle';
 
-export interface ITheme {
-	readonly type: ThemeType;
+	import * as platform from 'vs/platform/registry/common/platform';
 
-	/**
-	 * Resolves the color of the given color identifer. If the theme does not
-	 * specify the color, the default color is returned unless <code>useDefault</code> is set to false.
-	 * @param color the id of the color
-	 * @param useDefault specifies if the default color should be used. If not set, the default is used.
-	 */
-	getColor(color: ColorIdentifier, useDefault?: boolean): Color | null;
+	import { ColorIdentifier } from 'vs/platform/theme/common/colorRegistry';
 
-	/**
-	 * Returns wheter the theme defines a value for the color. If not, that means the
-	 * default color will be used.
-	 */
-	defines(color: ColorIdentifier): boolean;
-}
+	import { Event, Emitter } from 'vs/base/common/event';
 
-export interface IIconTheme {
-	readonly hasFileIcons: boolean;
-	readonly hasFolderIcons: boolean;
-	readonly hidesExplorerArrows: boolean;
-}
+	import { IEnvironmentService } from 'vs/platform/environment/common/environment';
 
-export interface ICssStyleCollector {
-	addRule(rule: string): void;
-}
+	
 
-export interface IThemingParticipant {
-	(theme: ITheme, collector: ICssStyleCollector, environment: IEnvironmentService): void;
-}
 
-export interface IThemeService {
-	_serviceBrand: any;
+	export const IThemeService = createDecorator<IThemeService>('themeService');
 
-	getTheme(): ITheme;
+	
 
-	readonly onThemeChange: Event<ITheme>;
 
-	getIconTheme(): IIconTheme;
+	export interface ThemeColor {
 
-	readonly onIconThemeChange: Event<IIconTheme>;
+		id: string;
 
-}
-
-// static theming participant
-export const Extensions = {
-	ThemingContribution: 'base.contributions.theming'
-};
-
-export interface IThemingRegistry {
-
-	/**
-	 * Register a theming participant that is invoked on every theme change.
-	 */
-	onThemeChange(participant: IThemingParticipant): IDisposable;
-
-	getThemingParticipants(): IThemingParticipant[];
-
-	readonly onThemingParticipantAdded: Event<IThemingParticipant>;
-}
-
-class ThemingRegistry implements IThemingRegistry {
-	private themingParticipants: IThemingParticipant[] = [];
-	private readonly onThemingParticipantAddedEmitter: Emitter<IThemingParticipant>;
-
-	constructor() {
-		this.themingParticipants = [];
-		this.onThemingParticipantAddedEmitter = new Emitter<IThemingParticipant>();
 	}
 
-	public onThemeChange(participant: IThemingParticipant): IDisposable {
-		this.themingParticipants.push(participant);
-		this.onThemingParticipantAddedEmitter.fire(participant);
-		return toDisposable(() => {
-			const idx = this.themingParticipants.indexOf(participant);
-			this.themingParticipants.splice(idx, 1);
-		});
+	
+
+
+	export function themeColorFromId(id: ColorIdentifier) {
+
+		return { id };
+
 	}
 
-	public get onThemingParticipantAdded(): Event<IThemingParticipant> {
-		return this.onThemingParticipantAddedEmitter.event;
+	
+
+
+	// theme icon
+
+	export interface ThemeIcon {
+
+		readonly id: string;
+
 	}
 
-	public getThemingParticipants(): IThemingParticipant[] {
-		return this.themingParticipants;
+	
+
+
+	export const FileThemeIcon = { id: 'file' };
+
+	export const FolderThemeIcon = { id: 'folder' };
+
+	
+
+
+	// base themes
+
+	export const DARK: ThemeType = 'dark';
+
+	export const LIGHT: ThemeType = 'light';
+
+	export const HIGH_CONTRAST: ThemeType = 'hc';
+
+	export type ThemeType = 'light' | 'dark' | 'hc';
+
+	
+
+
+	export function getThemeTypeSelector(type: ThemeType): string {
+
+		switch (type) {
+
+			case DARK: return 'vs-dark';
+
+			case HIGH_CONTRAST: return 'hc-black';
+
+			default: return 'vs';
+
+		}
+
 	}
-}
 
-let themingRegistry = new ThemingRegistry();
-platform.Registry.add(Extensions.ThemingContribution, themingRegistry);
+	
 
-export function registerThemingParticipant(participant: IThemingParticipant): IDisposable {
-	return themingRegistry.onThemeChange(participant);
-}
+
+	export interface ITheme {
+
+		readonly type: ThemeType;
+
+	
+
+
+		/**
+
+		 * Resolves the color of the given color identifer. If the theme does not
+
+		 * specify the color, the default color is returned unless <code>useDefault</code> is set to false.
+
+		 * @param color the id of the color
+
+		 * @param useDefault specifies if the default color should be used. If not set, the default is used.
+
+		 */
+
+		getColor(color: ColorIdentifier, useDefault?: boolean): Color | null;
+
+	
+
+
+		/**
+
+		 * Returns whether the theme defines a value for the color. If not, that means the
+
+		 * default color will be used.
+
+		 */
+
+		defines(color: ColorIdentifier): boolean;
+
+	}
+
+	
+
+
+	export interface IIconTheme {
+
+		readonly hasFileIcons: boolean;
+
+		readonly hasFolderIcons: boolean;
+
+		readonly hidesExplorerArrows: boolean;
+
+	}
+
+	
+
+
+	export interface ICssStyleCollector {
+
+		addRule(rule: string): void;
+
+	}
+
+	
+
+
+	export interface IThemingParticipant {
+
+		(theme: ITheme, collector: ICssStyleCollector, environment: IEnvironmentService): void;
+
+	}
+
+	
+
+
+	export interface IThemeService {
+
+		_serviceBrand: any;
+
+	
+
+
+		getTheme(): ITheme;
+
+	
+
+
+		readonly onThemeChange: Event<ITheme>;
+
+	
+
+
+		getIconTheme(): IIconTheme;
+
+	
+
+
+		readonly onIconThemeChange: Event<IIconTheme>;
+
+	
+
+
+	}
+
+	
+
+
+	// static theming participant
+
+	export const Extensions = {
+
+		ThemingContribution: 'base.contributions.theming'
+
+	};
+
+	
+
+
+	export interface IThemingRegistry {
+
+	
+
+
+		/**
+
+		 * Register a theming participant that is invoked on every theme change.
+
+		 */
+
+		onThemeChange(participant: IThemingParticipant): IDisposable;
+
+	
+
+
+		getThemingParticipants(): IThemingParticipant[];
+
+	
+
+
+		readonly onThemingParticipantAdded: Event<IThemingParticipant>;
+
+	}
+
+	
+
+
+	class ThemingRegistry implements IThemingRegistry {
+
+		private themingParticipants: IThemingParticipant[] = [];
+
+		private readonly onThemingParticipantAddedEmitter: Emitter<IThemingParticipant>;
+
+	
+
+
+		constructor() {
+
+			this.themingParticipants = [];
+
+			this.onThemingParticipantAddedEmitter = new Emitter<IThemingParticipant>();
+
+		}
+
+	
+
+
+		public onThemeChange(participant: IThemingParticipant): IDisposable {
+
+			this.themingParticipants.push(participant);
+
+			this.onThemingParticipantAddedEmitter.fire(participant);
+
+			return toDisposable(() => {
+
+				const idx = this.themingParticipants.indexOf(participant);
+
+				this.themingParticipants.splice(idx, 1);
+
+			});
+
+		}
+
+	
+
+
+		public get onThemingParticipantAdded(): Event<IThemingParticipant> {
+
+			return this.onThemingParticipantAddedEmitter.event;
+
+		}
+
+	
+
+
+		public getThemingParticipants(): IThemingParticipant[] {
+
+			return this.themingParticipants;
+
+		}
+
+	}
+
+	
+
+
+	let themingRegistry = new ThemingRegistry();
+
+	platform.Registry.add(Extensions.ThemingContribution, themingRegistry);
+
+	
+
+
+	export function registerThemingParticipant(participant: IThemingParticipant): IDisposable {
+
+		return themingRegistry.onThemeChange(participant);
+
+	}
